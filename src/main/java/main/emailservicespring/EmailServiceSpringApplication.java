eb5cc6be02cd7f8b3d3b501e98372d8a6277b945
illiamatsko <illia.matsko.shi.2023@lpnu.ua>
2024-04-27 15:42:51 +0300

    Initial commit

---
+++ src/main/java/main/emailservicespring/EmailServiceSpringApplication.java
@@ -0,0 +1,13 @@
+package main.emailservicespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailServiceSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailServiceSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/main/emailservicespring/EmailServiceSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package main.emailservicespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailServiceSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
